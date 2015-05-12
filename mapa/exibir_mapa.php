05b3adb8827cc97b1d3f8514f0e86387b5d26f9d
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-12 09:47:37 -0300

    Página do Mini Mapa adicionada

---
+++ mapa/exibir_mapa.php
@@ -0,0 +1,377 @@
+﻿<!DOCTYPE html PUBLIC "-f33//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+<style type="text/css">
+	html { height:100%; width:100%; }
+	body { height:100%; width:100%; margin:0px; padding:0px; font:13px/1.2 Helvetica,Arial,sans-serif; color: #000000}
+	#tibia_map { height:80%; width:100%; margin:0px; padding:0px; position:relative; }
+	#map_info { height:auto; width:798px; margin:5px auto; padding:0px; border:1px solid #A7D7F9; position:relative; }
+	table { height:auto; width:798px; margin:0px; padding:0px; border:0px solid; }
+	td { height:17px; width:798px; margin:0px; padding:0px; border:0px solid; font:inherit; }
+	a { font:inherit; text-decoration:none; }
+	#info_hide { border-bottom:1px solid #A7D7F9; text-align:center; }
+	.td1 { width:80px; }
+	.td2 { width:420px; padding:1px 3px 0px; }
+</style>
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+<script type="text/javascript">
+var MAP_VERSION = 6;
+var MAP_WIDTH = 8*256;
+var MAP_HEIGHT = 8*256;
+var ORIGIN_X = -143;
+var ORIGIN_Y = 543;
+var ORIGIN_Z = 7;
+var MAX_ZOOM = 5;
+var MIN_ZOOM = 1;
+var map_max = Math.max(MAP_WIDTH,MAP_HEIGHT);
+var map;
+var floor;
+var start_floor;
+var start_x;
+var start_y;
+var start_zoom;
+var linkMarker = null;
+
+var NPC = {};
+NPC['Mano Widin'] = [1045, 1601, 7];
+
+function LatLngToCoord(latLng) {
+	return new google.maps.Point(ORIGIN_X+Math.floor(latLng.lng()*map_max/2.56),ORIGIN_Y+Math.floor(latLng.lat()*map_max/2.56));
+}; 
+
+function CoordToLatLng(x,y) {
+	return new google.maps.LatLng((y-ORIGIN_Y+0.5)/map_max*2.56,(x-ORIGIN_X+0.5)/map_max*2.56);
+}; 
+
+function SelectElement(element) {
+	var r = document.createRange();
+	r.selectNodeContents(document.getElementById(element));
+	window.getSelection().addRange(r);
+}
+
+function CollapseTable() {
+	if (document.getElementById("info_table").style.display=="none") {
+		document.getElementById("info_table").style.display="inline";
+		var table=document.getElementById("info_hide");
+		table.innerHTML="Esconder";
+		table.style.borderBottom="1px #A7D7F9 solid";
+	} else {
+		document.getElementById("info_table").style.display="none";
+		var table=document.getElementById("info_hide");
+		table.innerHTML="Mostrar";
+		table.style.borderBottom="0px solid";
+	}
+	
+	SetSize();
+	MapResize();
+}
+
+function ChangeFloor(change) {
+	floor+=change;
+	
+	if (floor<0)
+		floor=0;
+	else if (floor>15)
+		floor=15;
+		
+	MapUpdate();
+	map.setMapTypeId(floor+"");
+	document.getElementById("floor_div").innerHTML = 7-floor;
+	
+	var backgroundColor = "#000000";
+	
+	if (floor==7)
+	backgroundColor = "#336699";
+	
+	document.getElementById("tibia_map").style.backgroundColor = backgroundColor;
+	
+	if (linkMarker) {
+		linkMarker.setMap(start_floor == floor?map:null);
+	}
+}; 
+
+function MapUpdate() {
+	var coord_x = LatLngToCoord(map.getCenter()).x;
+	var coord_y = LatLngToCoord(map.getCenter()).y;
+	var coord_z = floor;
+	var zoom = map.getZoom();
+	var url;
+	document.getElementById("info_coords").innerHTML = "x: "+coord_x+"&nbsp;&nbsp;&nbsp;y: "+coord_y+"&nbsp;&nbsp;&nbsp;z: "+floor;
+	document.getElementById("wiki").value=url = "{{mapa|"+coord_x+","+coord_y+","+floor+":"+zoom+"|aqui}}";
+	url="http://www.paulosalvatore.com.br/maruim/?p=mapa&x="+coord_x+"&y="+coord_y+"&z="+floor+"&zoom="+zoom;
+	document.getElementById("info_link").value=url;
+	document.getElementById("info_link").innerHTML=url;
+	zoom2 = Math.max(3,zoom+2);
+}; 
+
+function MapResize() {
+	var center = map.getCenter(); 
+	google.maps.event.trigger(map,"resize"); 
+	map.setCenter(center); 
+}
+
+function SetSize() {
+	document.getElementById("tibia_map").style.height=(document.body.offsetHeight-document.getElementById("map_info").offsetHeight-10)+"px";
+}
+
+function CheckLinkAndInitCoords() {
+var url = {};
+parent.document.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(m,key,value) {url[key] = value;});
+
+if (url.npc != undefined && parseInt(url.npc) != NaN){
+	url.npc = decodeURIComponent(url.npc);
+	start_x = parseInt(NPC[url.npc][0]);
+	start_y = parseInt(NPC[url.npc][1]);
+	start_floor = floor = NPC[url.npc][2];
+	start_zoom = 3;
+
+	var companyMarker = new google.maps.Marker({
+		position: CoordToLatLng(start_x, start_y),
+		map: map,
+		title:url.npc
+	});
+}else{
+	if (url.x != undefined && parseInt(url.x) != NaN)
+		start_x = parseInt(url.x);
+	else
+		start_x = ORIGIN_X+MAP_WIDTH/2;
+		
+	if (url.y != undefined && parseInt(url.y) != NaN)
+		start_y = parseInt(url.y);
+	else
+		start_y = ORIGIN_Y+MAP_HEIGHT/2;
+		
+	if (url.z != undefined && parseInt(url.z) != NaN && parseInt(url.z) >= 0 && parseInt(url.z) <= 15)
+		start_floor = floor = parseInt(url.z);
+	else
+		start_floor = floor = ORIGIN_Z;
+		
+	if (url.zoom != undefined && parseInt(url.zoom) != NaN)
+		start_zoom = parseInt(url.zoom);
+	else
+		start_zoom = 2;
+	}
+}
+
+function LoadMap() {
+	var backgroundColor = "#000000";
+	if(floor==7)
+		backgroundColor = "#00669C";
+	var mapOptions = {
+		backgroundColor: backgroundColor,
+		center: CoordToLatLng(start_x,start_y),
+		zoom: start_zoom,
+		mapTypeControlOptions: {
+			mapTypeIds: ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
+		},
+		disableDefaultUI: true,
+		overviewMapControl: false,
+		overviewMapControlOptions: {
+			opened: "true"
+		},
+		zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.LARGE
+		},
+		zoomControl: true
+	};
+	
+	var tibiaMapOptions = {
+		getTileUrl: function(coord,zoom) {
+			var max = Math.pow(2,zoom) - 1;
+			var x = coord.x;
+			var y = max - coord.y;
+			
+			if (x<0 || x>max || y<0 || y>max) {
+				if (floor == 7)
+					return "imagens/mapa/blue.png";
+				return "imagens/mapa/black.png";
+			}
+			return "tiles_maruim/"+ floor + "/" + zoom + "/" + x + "/" + y + ".png";
+		},
+		tileSize: new google.maps.Size(256,256),
+		isPng: true,
+		maxZoom: MAX_ZOOM,
+		minZoom: MIN_ZOOM
+	};
+
+	map = new google.maps.Map(document.getElementById("tibia_map"),mapOptions);
+
+	var tibiaMapType;
+	for (var i=0;i<=15;i++) {
+		tibiaMapType = new google.maps.ImageMapType(tibiaMapOptions);
+		tibiaMapType.projection = new SimpleProjection();
+		map.mapTypes.set(i+"",tibiaMapType);
+	}
+	map.setMapTypeId(floor+"");
+	google.maps.event.addListener(map,"bounds_changed",MapUpdate);
+	google.maps.event.addDomListener(window,"resize",MapResize);
+	
+	if (start_x != ORIGIN_X+MAP_WIDTH/2) {
+		linkMarker = new google.maps.Marker({
+			map: map,
+			position: CoordToLatLng(start_x,start_y),
+			title: "Duplo click para remover.",
+			animation: google.maps.Animation.BOUNCE
+		});
+		
+		setTimeout(function() {
+			linkMarker.setAnimation(null);
+		}, 3000);
+		
+		google.maps.event.addListener(linkMarker, "dblclick", function() {
+			linkMarker.setMap(null);
+			linkMarker = null;
+		});
+	}
+}
+
+function FloorControl() {
+	var controlDiv = document.createElement("DIV");
+	controlDiv.style.margin = "0px";
+	controlDiv.style.padding = "0px";
+	
+	var upDiv = document.createElement("DIV");
+	upDiv.style.cursor="pointer";
+	upDiv.style.width = "20px";
+	upDiv.style.height = "20px";
+	upDiv.style.margin = "0px";
+	upDiv.style.padding = "5px 6px";
+	upDiv.title = "+1";
+	upDiv.style.background = "url('http://www.tibiawiki.com.br/up.png') no-repeat center";
+	controlDiv.appendChild(upDiv);
+	
+	var floorDiv = document.createElement("DIV");
+	floorDiv.style.width = "20px";
+	floorDiv.style.height = "21px";
+	floorDiv.style.margin = "0px 6px";
+	floorDiv.style.padding = "0px";
+	floorDiv.style.pointerEvents = "none";
+	floorDiv.style.background = "url('http://www.tibiawiki.com.br/floor.png') no-repeat center";
+	controlDiv.appendChild(floorDiv);
+	
+	var textDiv = document.createElement("DIV");
+	textDiv.id = "floor_div";
+	textDiv.style.width = "18px";
+	textDiv.style.height = "21px";
+	textDiv.style.margin = "0px";
+	textDiv.style.padding = "1px 0px";
+	textDiv.style.textAlign = "center";
+	textDiv.style.color = "red";
+	textDiv.style.font = "bold 15px Helvetica,Arial,sans-serif";
+	textDiv.innerHTML = 7-floor;
+	floorDiv.appendChild(textDiv);
+	
+	var downDiv = document.createElement("DIV");
+	downDiv.style.cursor="pointer";
+	downDiv.style.width = "20px";
+	downDiv.style.height = "20px";
+	downDiv.style.margin = "0px 0px";
+	downDiv.style.padding = "5px 6px";
+	downDiv.title = "-1";
+	downDiv.style.background = "url('http://www.tibiawiki.com.br/down.png') no-repeat center";
+	controlDiv.appendChild(downDiv);
+	
+	var centerDiv = document.createElement("DIV");
+	centerDiv.style.cursor="pointer";
+	centerDiv.style.width = "20px";
+	centerDiv.style.height = "20px";
+	centerDiv.style.margin = "0px 0px";
+	centerDiv.style.padding = "5px 6px";
+	centerDiv.title = "Centro";
+	centerDiv.style.background = "url('http://www.tibiawiki.com.br/center.png') no-repeat center";
+	controlDiv.appendChild(centerDiv);
+	
+	var CENTER = CoordToLatLng(start_x,start_y);
+	google.maps.event.addDomListener(upDiv,"click",function() {ChangeFloor(-1)});
+	google.maps.event.addDomListener(downDiv,"click",function() {ChangeFloor(+1)});
+	google.maps.event.addDomListener(centerDiv,"click",function() {map.setCenter(CENTER)});
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv);
+}
+
+function Crosshair() {
+	if (map.controls[google.maps.ControlPosition.TOP_CENTER].getLength()==0) {
+		var xDiv = document.createElement("DIV");
+		xDiv.style.width = "100%";
+		xDiv.style.height = "1px";
+		xDiv.style.background = "url('http://www.tibiawiki.com.br/cx.png') repeat-x center";
+		xDiv.style.pointerEvents = "none";
+		map.controls[google.maps.ControlPosition.LEFT_CENTER].push(xDiv);
+		
+		var yDiv = document.createElement("DIV");
+		yDiv.style.width = "1px";
+		yDiv.style.height = "100%";
+		yDiv.style.background = "url('http://www.tibiawiki.com.br/cy.png') repeat-y center";
+		yDiv.style.pointerEvents = "none";
+		map.controls[google.maps.ControlPosition.TOP_CENTER].push(yDiv);
+	}else{
+		map.controls[google.maps.ControlPosition.LEFT_CENTER].clear();
+		map.controls[google.maps.ControlPosition.TOP_CENTER].clear();
+	}
+};
+
+function SimpleProjection() {
+};
+
+SimpleProjection.prototype.fromLatLngToPoint = function(latLng) {
+	return new google.maps.Point(latLng.lng()*100,latLng.lat()*100);
+};
+
+SimpleProjection.prototype.fromPointToLatLng = function(point,noWrap) { 
+	return new google.maps.LatLng(point.y/100,point.x/100);
+};
+
+function Init() {
+	SetSize();
+	CheckLinkAndInitCoords();
+	LoadMap();
+	FloorControl();
+	Crosshair();
+}
+
+</script>
+</head>
+
+<body onload="Init()">
+	<div id="tibia_map"></div>
+	<div id="map_info">
+	
+	<table>
+		<tr>
+			<td id="info_hide" onclick="CollapseTable();">Esconder</td>
+		</tr>
+	</table>
+	
+	<table id="info_table">
+		<tr>
+			<td class="td1">
+				<input id="crosshair_checkbox" type="checkbox" value="1" checked="checked" onclick="Crosshair();" />Cursor
+			</td>
+		</tr>
+		
+		<tr>
+			<td class="td1">Coordenadas:</td>
+			<td class="td2" id="info_coords" onclick="SelectElement('info_coords');"></td>
+		</tr>
+		<tr>
+			<td class="td1" onclick="SelectElement('info_link');">Código Local:</td>
+			<td class="td2" onclick="SelectElement('info_link2');">
+				<input id="wiki" readonly="readonly" type="text" value="" size="45" style="background:#FFFFFF; color:#000000; padding-left: 1px; border: 1px solid #A7D7F9; -moz-border-radius: 2px; border-radius: 2px;" onclick="this.select()" />
+			</td>
+		</tr>
+		<tr>
+			<td class="td1" onclick="SelectElement('info_link');" valign="top">Link direto:</td>
+			<td class="td2" onclick="SelectElement('info_link2');">
+				<input id="info_link" readonly="readonly" type="text" value="" size="45" style="background:#FFFFFF; color:#000000; padding-left: 1px; border: 1px solid #A7D7F9; -moz-border-radius: 2px; border-radius: 2px;" onclick="this.select()" />
+			</td>
+		</tr>
+	</table>
+	</div>
+	
+	<script type="text/javascript">SetSize();</script>
+	
+	</body>
+</html>
---
+++ mapa/script_posicionamento_imagens.php
@@ -0,0 +1,182 @@
+﻿<?php
+	set_time_limit(60*60*60);
+	$diretorioOriginal = "tiles_maruim_original/";
+	$diretorio = "tiles_maruim/";
+	for($a=7;$a<=7;$a++){
+		$pasta1 = $diretorio.$a;
+		for($b=1;$b<=5;$b++){
+			$imagem = 1;
+			$limite = pow(2, $b);
+			$pasta2 = $pasta1."/".$b;
+			for($c=0;$c<$limite;$c++){
+				$x = 0;
+				for($d=0;$d<$limite;$d++){
+					$y = $limite-$c-1;
+					$pasta1 = $diretorio.$a."/";
+					$pasta2 = $pasta1.$b."/";
+					$pasta3 = $pasta2.$x;
+					$exibirImagem = $imagem;
+					if($imagem < 10)
+						$exibirImagem = "0".$imagem;
+					$s = 256*$limite;
+					$nomeArquivoImagemOriginal = "MiniMap_".$s."/MiniMap_".$s."_".$exibirImagem.".png";
+					if(!file_exists($pasta1))
+						mkdir($pasta1);
+					if(!file_exists($pasta2))
+						mkdir($pasta2);
+					if(!file_exists($pasta3))
+						mkdir($pasta3);
+					$nomeArquivoImagem = "$pasta3/$y.png";
+					$arquivoOriginal = $diretorioOriginal.$nomeArquivoImagemOriginal;
+					if(filesize($arquivoOriginal) != 1517)
+						copy($arquivoOriginal, $nomeArquivoImagem);
+					$x++;
+					$imagem++;
+				}
+			}
+		}
+	}
+	echo'
+		<style>
+			body {
+				margin: 0;
+			}
+			.x_y {
+				position: absolute;
+				margin-top: -20px;
+				margin-left: 5px;
+				color: white;
+				font-family: Verdana;
+			}
+			.principal {
+				margin-bottom: 7px;
+				display: none;
+			}
+		</style>
+		<table class="principal" cellpadding="0" cellspacing="0">
+			<tr>
+				<td>
+					<img src="tiles/7-1-0-1.png" /><br>
+					<div class="x_y">x0 y1 - 01</div>
+				</td>
+				<td>
+					<img src="tiles/7-1-1-1.png" /><br>
+					<div class="x_y">x1 y1 - 02</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="tiles/7-1-0-0.png" /><br>
+					<div class="x_y">x0 y0 - 03</div>
+				</td>
+				<td>
+					<img src="tiles/7-1-1-0.png" /><br>
+					<div class="x_y">x1 y0 - 04</div>
+				</td>
+			</tr>
+		</table>
+		<table cellpadding="0" cellspacing="0">
+			<tr>
+				<td>
+					<img src="tiles/7-2-0-3.png" /><br>
+					<div class="x_y">x0 y3 - 01</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-1-3.png" /><br>
+					<div class="x_y">x1 y3 - 02</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-2-3.png" /><br>
+					<div class="x_y">x2 y3 - 03</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-3-3.png" /><br>
+					<div class="x_y">x3 y3 - 04</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="tiles/7-2-0-2.png" /><br>
+					<div class="x_y">x0 y2 - 05</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-1-2.png" /><br>
+					<div class="x_y">x1 y2 - 06</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-2-2.png" /><br>
+					<div class="x_y">x2 y2 - 07</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-3-2.png" /><br>
+					<div class="x_y">x3 y2 - 08</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="tiles/7-2-0-1.png" /><br>
+					<div class="x_y">x0 y1 - 09</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-1-1.png" /><br>
+					<div class="x_y">x1 y1 - 10</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-2-1.png" /><br>
+					<div class="x_y">x2 y1 - 11</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-3-1.png" /><br>
+					<div class="x_y">x3 y1 - 12</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="tiles/7-2-0-0.png" /><br>
+					<div class="x_y">x0 y0 - 13</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-1-0.png" /><br>
+					<div class="x_y">x1 y0 - 14</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-2-0.png" /><br>
+					<div class="x_y">x2 y0 - 15</div>
+				</td>
+				<td>
+					<img src="tiles/7-2-3-0.png" /><br>
+					<div class="x_y">x3 y0 - 16</div>
+				</td>
+			</tr>
+		</table>
+	';
+	// set_time_limit(60*60*60);
+	// $diretorioOriginal = "tiles_maruim_original/";
+	// $diretorio = "tiles_maruim/";
+	// for($a=7;$a<=7;$a++){
+		// $pasta1 = $diretorio.$a;
+		// for($b=1;$b<=2;$b++){
+			// $limite = pow(2, $b);
+			// $pasta2 = $pasta1."/".$b;
+			// for($c=0;$c<$limite;$c++){
+				// for($d=0;$d<$limite;$d++){
+					// $pasta3 = $pasta2."/".$d;
+					// $n = $c*$limite+$d+1;
+					// $s = 256*$limite;
+					// if($n < 10)
+						// $n = "0$n";
+					// $nomeArquivoImagemOriginal = $s."_tiles/MiniMapa_".$s."_".$n.".png";
+					// if(!file_exists($pasta1))
+						// mkdir($pasta1);
+					// if(!file_exists($pasta2))
+						// mkdir($pasta2);
+					// if(!file_exists($pasta3))
+						// mkdir($pasta3);
+					// $nomeArquivoImagem = "$pasta3/".($limite-$c).".png";
+					// $arquivoOriginal = $diretorioOriginal.$nomeArquivoImagemOriginal;
+					// $arquivo = copy($arquivoOriginal, $nomeArquivoImagem);
+				// }
+			// }
+		// }
+	// }
+?>
