8e6398ca6c95ea6d3ff2713ebbbb4c92d8c57ca9
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-12 16:58:39 -0300

    Alterações na Tabela de Experiência
    Diversas verificações na montagem foram incluídas;
    Opção para modificar o nível máximo da tabela adicionada;
    Alterada a forma que a tabela é estruturada/montada;
    Otimização geral do código.

--- js/paginas/tabela_experiencia.js
+++ js/paginas/tabela_experiencia.js
@@ -1,6 +1,19 @@
 function construirTabelaExperiencia(){
-	var levelMaximo = 1000;
-	tabela = '\
+	var nivelMaximoPermitido = 1000;
+	var nivelMinimo = 4;
+	var nivelMaximo = parseInt($("input:text.nivelMaximo").val());
+	if(nivelMaximo > nivelMaximoPermitido)
+		nivelMaximo = nivelMaximoPermitido;
+	if(nivelMaximo%4 > 0)
+		nivelMaximo = nivelMaximo-nivelMaximo%4+4;
+	else
+		nivelMaximo = nivelMaximo-nivelMaximo%4;
+	if((isNaN(nivelMaximo)) || (nivelMaximo < nivelMinimo))
+		nivelMaximo = nivelMinimo;
+	if($("#tabelaExperiencia").data("nivel") == nivelMaximo)
+		return false;
+	var limiteNivel = nivelMaximo/4;
+	var tabelaExperiencia = '\
 		<table class="tabela" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed; border-spacing: 0px;">\
 			<tr class="cabecalho">\
 				<td colspan="4">\
@@ -9,45 +22,54 @@ function construirTabelaExperiencia(){
 			</tr>\
 			<tr class="conteudo">\
 				';
-				for(i=0;i<4;i++){
-					tabela += '\
-						<td>\
-							<table class="tabela" cellpadding="0" cellspacing="0" width="100%">\
-								<tr class="item">\
-									<td>\
-										<b>Nível</b>\
-									</td>\
-									<td>\
-										<b>Experiência</b>\
-									</td>\
-								</tr>\
-								';
-								for(j=1;j<=levelMaximo/4;j++){
-									nivelAtual = i*levelMaximo/4+j;
-									experiencia = Math.round(50/3*(Math.pow(nivelAtual, 3)-6*Math.pow(nivelAtual, 2)+17*nivelAtual-12));
-									tabela += '\
-										<tr class="item">\
-											<td>\
-												'+nivelAtual+'\
-											</td>\
-											<td>\
-												'+experiencia+'\
-											</td>\
-										</tr>\
-									';
-								}
-								tabela += '\
-							</table>\
-						</td>\
+				for(i=1;i<=nivelMaximo;i++){
+					if((i == 1) || ((i-1)%limiteNivel == 0))
+						tabelaExperiencia += '\
+							<td style="vertical-align: top;">\
+								<table class="tabela" cellpadding="0" cellspacing="0" width="100%">\
+									<tr class="item">\
+										<td>\
+											<b>Nível</b>\
+										</td>\
+										<td>\
+											<b>Experiência</b>\
+										</td>\
+									</tr>\
+						';
+					nivelAtual = i;
+					experiencia = Math.round(50/3*(Math.pow(nivelAtual, 3)-6*Math.pow(nivelAtual, 2)+17*nivelAtual-12));
+					tabelaExperiencia += '\
+						<tr class="item">\
+							<td>\
+								'+nivelAtual+'\
+							</td>\
+							<td>\
+								'+experiencia+'\
+							</td>\
+						</tr>\
 					';
+					if(((i%limiteNivel) == 0) || i == nivelMaximo)
+						tabelaExperiencia += '\
+								</table>\
+							</td>\
+						';
 				}
-				tabela += '\
+				tabelaExperiencia += '\
 			</tr>\
 		</table>\
 	';
-	return tabela;
+	$("#tabelaExperiencia").html(tabelaExperiencia).data("nivel", nivelMaximo);
 }
 $(function(){
-	var tabelaExperiencia = construirTabelaExperiencia();
-	$("#tabelaExperiencia").html(tabelaExperiencia);
-})
+	$(".botao.nivelMaximo").click(function(){
+		construirTabelaExperiencia();
+	});
+	$("input:text.nivelMaximo").keydown(function(event){
+		if(event.keyCode == 13)
+			$(".botao.nivelMaximo").click();
+	});
+	$("select.nivelMaximo").change(function(){
+		$("input:text.nivelMaximo").val($(this).val());
+		$(".botao.nivelMaximo").click();
+	}).change();
+});
--- paginas/tabela_experiencia.php
+++ paginas/tabela_experiencia.php
@@ -1,10 +1,21 @@
 <?php
+	$niveisSelecao = array(200, 400, 600, 800, 1000);
 	$conteudo_pagina .= '
 		<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="'.$incluir_arquivo.'">
 			<div class="conteudo_box pagina padding">
 				Esta é uma lista dos pontos de experiência que são necessários para avançar para os vários níveis. Lembre-se que você também pode verificar a respectiva barra de habilidade na sua janela de habilidade do cliente para verificar o seu progresso em direção ao próximo nível.<br>
 				<br>
-				<div id="tabelaExperiencia"></div>
+				<b>Selecione o nível máximo da tabela:</b><br>
+				<select class="nivelMaximo" style="width: 120px; margin: 3px 0 3px 0;">
+					';
+					foreach($niveisSelecao as $nivel)
+						$conteudo_pagina .= '<option value="'.$nivel.'">Nível '.$nivel.'</option>';
+					$conteudo_pagina .= '
+				</select><br>
+				<input type="text" class="nivelMaximo" style="width: 116px; margin-bottom: 3px;"><br>
+				<input type="button" class="botao nivelMaximo" value="Aplicar" style="margin-bottom: 3px;">
+				<br>
+				<div id="tabelaExperiencia" data-nivel="0"></div>
 			</div>
 		</div>
 	';
