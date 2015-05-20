bf36d133a464025d5a9faad8d96d8648ad4ee3e4
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-20 12:32:56 -0300

    Arquivo de Configuração 'Registrar Conta'
    Arquivo em desenvolvimento

---
+++ includes/config_registrar_conta.php
@@ -0,0 +1,33 @@
+<?php
+	// include("includes/classes/ClassPersonagem.php");
+	// $ClassPersonagem = new Personagem();
+	$formulario_registrar_conta = array(
+		array(
+			// "nome_personagem" => array(
+				// "exibicao" => "Nome do Personagem",
+				// "tipo" => "texto",
+				// "size" => 25,
+				// "minlength" => 3,
+				// "maxlength" => 30,
+				// "tipo_dados" => array(
+					// "numeros" => true,
+					// "letras" => true,
+					// "simbolos" => false,
+				// ),
+				// "tipo_dados_obrigatorio" => array(
+					// "numeros" => false,
+					// "letras" => false,
+					// "email" => false,
+				// ),
+				// "sugestao" => true,
+				// "obrigatorio" => true,
+				// "unico" => true,
+				// "tabela_verificacao" => "players",
+				// "campo_verificacao" => "name",
+				// "igual" => "",
+				// "msg_unico" => "Esse nome já está em uso. Por favor digite outro!",
+				// "msg_igual" => ""
+			// ),
+		)
+	);
+?>
