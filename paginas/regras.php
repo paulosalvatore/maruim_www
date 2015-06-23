3ba8e6d603912dfda486331ffa5b24bc7fb97fff
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-23 19:37:17 -0300

    Esboço da Página de Regras adicionado

--- paginas/regras.php
+++ paginas/regras.php
@@ -1,4 +1,5 @@
 <?php
+$nomeServidor = "nomeServidorOT";
 	$conteudo_pagina .= '
 		<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="'.$incluir_arquivo.'">
 			<div class="conteudo_box pagina">
@@ -7,11 +8,93 @@
 				</div>
 				<div class="box_frame_conteudo_principal" carregar_box="1">
 					<div class="box_frame_conteudo">
-						<ul class="tarefas">
-							<li>
-								Regras
-							</li>
-						</ul>
+						'.$nomeServidor.' é um jogo online onde jogadores de todo o mundo se encontram diariamente. Para que este jogo se mantenha divertido e justo para todos, o '.$nomeServidor.' aponta gamemasters que têm poder de parar atitudes destrutivas no jogo ou no website oficial, penalisando, banindo ou deletando personagens e contas.<br>
+						<br>
+						Desta forma, o '.$nomeServidor.' considera as seguintes ações proibidas:<br>
+						<br>
+						<br>
+						1- Nomes<br>
+						<br>
+						a) Nome Ofensivo<br>
+						<br>
+						Nomes relacionados a sexo e/ou drogas, ou contendo insultos e/ou racismo.<br>
+						<br>
+						b) Formato Inválido<br>
+						<br>
+						Nomes com combinações de letras e/ou frases sem sentido.<br>
+						<br>
+						c) Nome Inadequado<br>
+						<br>
+						Nomes que fujam da temática medieval do '.$nomeServidor.', ou que contenham anúncios ilegais.<br>
+						<br>
+						d) Nome com Posições Oficiais ou Referente à Violação das Regras<br>
+						<br>
+						Nomes relacionados à administração do '.$nomeServidor.', ou que incentivem a violação das regras.<br>
+						<br>
+						<br>
+						2- Mensagens e Declarações<br>
+						<br>
+						a) Ofensas<br>
+						<br>
+						Declarações insultuosas, racistas, relacionadas a sexo e/ou drogas, entre outros, principalmente no Help Channel.<br>
+						<br>
+						b) Spam<br>
+						<br>
+						Repetir a mesma palavra ou oração várias vezes em canais públicos ou no Default, exceto no Trade Channel.<br>
+						<br>
+						c) Anúncios Ilegais<br>
+						<br>
+						Anúncios de outros servidores, sites, jogos ou qualquer conteúdo não relacionado ao '.$nomeServidor.'.<br>
+						<br>
+						d) Estimular a Violação das Regras<br>
+						<br>
+						Declarações que citem ou estimulem a violação das regras.<br>
+						<br>
+						<br>
+						3- Trapaças<br>
+						<br>
+						a) Abuso de Bug<br>
+						<br>
+						Explorar qualquer erro do jogo para se beneficiar. Se você encontrar qualquer erro, ele deve ser reportado imediatamente.<br>
+						<br>
+						b) Abuso de Fraquezas do Jogo<br>
+						<br>
+						Abusar intencionalmente de fraquezas do jogo.<br>
+						<br>
+						c) Programas Não-Oficiais<br>
+						<br>
+						Utilizar qualquer macro ou programa que automatize as ações do personagem, sem estar ativo no cliente (Bot AFK).<br>
+						<br>
+						Utilizar mage-bomb ou qualquer programa semelhante.<br>
+						<br>
+						d) Hacking<br>
+						<br>
+						Tentar roubar a conta de outro jogador.<br>
+						<br>
+						<br>
+						4- Gamemasters<br>
+						<br>
+						a) Ameaçar um Gamemaster<br>
+						<br>
+						Ameaçar ou ofender qualquer gamemaster por causa de suas ações.<br>
+						<br>
+						b) Fingir ter Influência na Execução das Regras<br>
+						<br>
+						Fingir ser um gamemaster ou tentar influenciar suas decisões.<br>
+						<br>
+						c) Falsa Denúncia<br>
+						<br>
+						Enviar propositalmente informações fictícias ou erradas para um gamemaster, com a finalidade de prejudicá-lo.<br>
+						<br>
+						<br>
+						5- Comportamento Destrutivo<br>
+						<br>
+						Qualquer atitude que não esteja citada nas regras acima, mas que seja analisada e considerada prejudicial ao jogo por qualquer membro da equipe '.$nomeServidor.'.<br>
+						<br>
+						<br>
+						A violação de qualquer uma das regras acima pode acarretar em um banimento ou até mesmo na exclusão da conta do personagem.<br>
+						<br>
+						Estas regras poderão mudar a qualquer momento. Qualquer mudança será comunicada.<br><br>
 					</div>
 				</div>
 			</div>
