51239bb62d09dfaa17b3cb1c89f32a445049791f
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-23 21:15:05 -0300

    Criação da Barra Direita (incompleta)

--- barra_direita.php
+++ barra_direita.php
@@ -1,5 +1,15 @@
 <?php
 	$conteudo_barra_direita .= '
-		
+		<div id="boxPedestalOnline">
+			<img id="monstroSemana" src="imagens/criaturas/Demon.gif" onClick="window.location = \'?p=criaturas-Demon\';" alt="Monstro da Semana" />
+			<img id="pedestalOnline" src="imagens/barra_direita/pedestal_online.png" alt="" />
+			<div id="jogadoresOnline" onClick="window.location = \'?p=jogadores_online\';">'.$ClassFuncao->exibirNumeroJogadoresOnline().'</div>
+		</div>
+		<div id="premiumBox">
+			<img src="imagens/barra_direita/premiumbox.gif" alt="" /><br>
+			<div id="adquirirPremium">
+				<input type="button" class="botao_verde" value="adquirir" onClick="document.location = \'?p=adquirir_premium\'" />
+			</div>
+		</div>
 	';
 ?>
