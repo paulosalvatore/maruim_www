59aa1a6e12b57924866672cde7fc09725c63ffe1
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-25 20:57:03 -0300

    Páginas 'NPCs' criada

---
+++ includes/classes/ClassNpcs.php
@@ -0,0 +1,168 @@
+<?php
+	class Npcs {
+		public function pegarInfoNpc($npcId){
+			$npc = array();
+			$queryNpc = mysql_query("SELECT * FROM z_npcs WHERE (id LIKE '$npcId')");
+			while($resultadoNpc = mysql_fetch_assoc($queryNpc))
+				$npc = $resultadoNpc;
+			if(count($npc) > 0)
+				$npc["imagem"] = $this->pegarImagemNpc($npc);
+			return $npc;
+		}
+		public function exibirItensNpc($npcId){
+			include("includes/classes/ClassItens.php");
+			$ClassItens = new Itens();
+			$itens = array("c" => array(), "v" => array());
+			$queryItensNpc = mysql_query("SELECT * FROM z_npcs_itens WHERE (npc LIKE '$npcId')");
+			while($resultadoItensNpc = mysql_fetch_assoc($queryItensNpc)){
+				$itens[$resultadoItensNpc["acao"]][] = array(
+					"item" => $resultadoItensNpc["item"],
+					"valor" => $resultadoItensNpc["valor"]
+				);
+			}
+			$buscarItens = array();
+			foreach(array_merge($itens["c"], $itens["v"]) as $item)
+				if(!in_array($item, $buscarItens))
+					$buscarItens[] = $item["item"];
+			$itensInfo = $ClassItens->getItemInfoSQL($buscarItens);
+			$exibirItensCompra = '';
+			$exibirItensVenda = '';
+			$exibirItensNada = '
+					<tr class="item" align="center">
+						<td colspan="2" height="40">
+							<b>Nada.</b>
+						</td>
+					</tr>
+				';
+			$cabecalhoNpc = '
+				<tr class="cabecalho" align="center">
+					<td>
+						<b>Item</b>
+					</td>
+					<td>
+						<b>Valor</b>
+					</td>
+				</tr>
+			';
+			if(count($itens["c"]) == 0)
+				$exibirItensCompra .= $exibirItensNada;
+			if(count($itens["v"]) == 0)
+				$exibirItensVenda .= $exibirItensNada;
+			$itensNome = array();
+			foreach($itens["c"] as $c => $npcItem)
+				$itensNome[$c] = $itensInfo[$npcItem["item"]]["nome"];
+			asort($itensNome);
+			foreach($itensNome as $c => $itemId)
+				$exibirItensCompra .= $this->formatarItemNpc($itensInfo[$itens["c"][$c]["item"]], $itens["c"][$c]["valor"]);
+			$itensNome = array();
+			foreach($itens["v"] as $c => $npcItem)
+				$itensNome[$c] = $itensInfo[$npcItem["item"]]["nome"];
+			asort($itensNome);
+			foreach($itensNome as $c => $itemId)
+				$exibirItensVenda .= $this->formatarItemNpc($itensInfo[$itens["v"][$c]["item"]], $itens["v"][$c]["valor"]);
+			return array($exibirItensCompra, $exibirItensVenda);
+		}
+		public function formatarItemNpc($item, $valor){
+			return '
+				<tr class="item" align="center">
+					<td>
+						<a href="'.$item["url"].'">'.$item["nome"].'</a>
+					</td>
+					<td>
+						'.$valor.' gp
+					</td>
+				</tr>
+			';
+		}
+		public function pegarLinkNpc($npcId, $npcNome = ""){
+			if(empty($npcNome))
+				$npcNome = $this->pegarInfoNpc($npcId)["nome"];
+			return "?p=npcs-".$npcId."-".urlencode($npcNome);
+		}
+		public function pegarImagemNpc($npc){
+			$arquivoImagem = 'includes/classes/ClassOutfit.php?id='.$npc["lookType"].'&head='.$npc["lookHead"].'&body='.$npc["lookBody"].'&legs='.$npc["lookLegs"].'&feet='.$npc["lookFeet"].'&addons='.$npc["lookAddons"].'&mount='.$npc["lookMount"];
+			$tamanhoImagem = getimagesize('http://'.$_SERVER['HTTP_HOST'].'/'.$arquivoImagem);
+			$estilos = array();
+			if($npc["lookMount"] == 0)
+				$estilos[] = 'semMontaria';
+			if($tamanhoImagem[0] == 64)
+				$estilos[] = 'largura64';
+			else
+				$estilos[] = 'largura32';
+			return '
+				<div class="imagemNpc">
+					<img src="'.$arquivoImagem.'" alt="'.$npc["nome"].'" title="'.$npc["nome"].'" class="'.implode(" ", $estilos).'" />
+				</div>
+			';
+		}
+		public function pegarBuscaNpc($busca = ""){
+			return '
+				<form method="POST" action="?p=npcs-buscar">
+				<table class="tabela dark" cellpadding="0" cellspacing="0" width="100%">
+					<tr class="cabecalho">
+						<td>
+							Buscar NPCs
+						</td>
+					</tr>
+					<tr class="item">
+						<td>
+							Nome do NPC: <input type="text" name="busca" id="buscar_npcs" value="'.$busca.'"> <input type="submit" class="botao" value="Procurar">
+						</td>
+					</tr>
+				</table>
+				</form>
+				<br>
+			';
+		}
+		public function exibirBuscaNpc($busca){
+			$checarBuscaExata = mysql_fetch_array(mysql_query("SELECT COUNT(*) as total FROM z_npcs WHERE (nome LIKE '$busca')"));
+			$checarBuscaExata = $checarBuscaExata["total"];
+			$queryBusca = $busca;
+			if($checarBuscaExata != 1)
+				$queryBusca = "%$busca%";
+			$npcs = array();
+			$queryBuscaNpc = mysql_query("SELECT * FROM z_npcs WHERE (nome LIKE '$queryBusca')");
+			while($resultadoBuscaNpc = mysql_fetch_assoc($queryBuscaNpc))
+				$npcs[] = $resultadoBuscaNpc;
+			if(count($npcs) == 1){
+				header("Location: ".$this->pegarLinkNpc($npcs[0]["id"], $npcs[0]["nome"]));
+				exit;
+			}
+			$exibirBusca .= '
+				'.$this->pegarBuscaNpc($busca).'
+				<table class="tabela odd" cellpadding="0" cellspacing="0" width="100%">
+					<tr class="cabecalho">
+						<td colspan="2">
+							Resultados da Busca por "'.$busca.'"
+						</td>
+					</tr>
+					';
+					foreach($npcs as $npc){
+						$linkNpc = $this->pegarLinkNpc($npc["id"], $npc["nome"]);
+						$exibirBusca .= '
+							<tr class="item">
+								<td width="30" align="center">
+									<a href="'.$linkNpc.'">'.$this->pegarImagemNpc($npc).'</a>
+								</td>
+								<td>
+									<a href="'.$linkNpc.'">'.$npc["nome"].'</a>
+								</td>
+							</tr>
+						';
+					}
+					if(count($npcs) == 0)
+						$exibirBusca .= '
+							<tr class="item">
+								<td colspan="2" height="100" align="center">
+									<b>Sua busca não encontrou nenhum resultado.</b>
+								</td>
+							</tr>
+						';
+					$exibirBusca .= '
+				</table>
+				<br>					
+			';
+			return $exibirBusca;
+		}
+	}
+?>
---
+++ paginas/npcs.php
@@ -0,0 +1,98 @@
+<?php
+	include("includes/classes/ClassNpcs.php");
+	$ClassNpcs = new Npcs();
+	$exibirBuscaNpc = $ClassNpcs->pegarBuscaNpc();
+	if($id == "buscar"){
+		$conteudo_pagina .= '
+			<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="npcs_buscar">
+				<div class="conteudo_box pagina">
+					'.$ClassNpcs->exibirBuscaNpc($_POST["busca"]).'
+				</div>
+			</div>
+		';
+	}
+	elseif(is_numeric($id)){
+		$npc = $ClassNpcs->pegarInfoNpc($id);
+		if(count($npc) > 0){
+			$conteudo_pagina .= '
+				<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="'.$incluir_arquivo.'">
+					<div class="conteudo_box pagina">
+						<div class="setas">
+							<div class="seta voltar">
+								<a href="?p=npcs"><img src="imagens/corpo/arrow_up.gif" /> voltar</a>
+							</div>
+						</div>
+						'.$exibirBuscaNpc.'
+						<div class="box_frame" carregar_box="1">
+							'.$npc["nome"].'
+						</div>
+						<br>
+						<table class="tabela odd" cellpadding="0" cellspacing="0" width="100%">
+							<tr class="cabecalho">
+								<td colspan="2">
+									Informações gerais
+								</td>
+							</tr>
+							<tr class="item">
+								<td width="100" align="right">
+									'.$npc["imagem"].'
+								</td>
+								<td>
+									<b>'.$npc["nome"].'</b>
+								</td>
+							</tr>
+							<tr class="item">
+								<td width="100" align="right">
+									<b>Localização</b>
+								</td>
+								<td>
+									<a href="?p=mapa&x='.$npc["posx"].'&y='.$npc["posy"].'&z='.$npc["posz"].'&zoom=5">Clique aqui para visualizar</a>
+								</td>
+							</tr>
+						</table>
+						<br>
+						';
+						$exibirItensNpc = $ClassNpcs->exibirItensNpc($id);
+						$exibirItensCompra = $exibirItensNpc[0];
+						$exibirItensVenda = $exibirItensNpc[1];
+						$conteudo_pagina .= '
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tr>
+								<td width="50%" valign="top">
+									<table class="tabela odd" cellpadding="0" cellspacing="0" width="100%">
+										<tr class="cabecalho" align="center">
+											<td colspan="2">
+												Vende:
+											</td>
+										</tr>
+										'.$exibirItensCompra.'
+									</table>
+								</td>
+								<td width="50%" valign="top">
+									<table class="tabela odd" cellpadding="0" cellspacing="0" width="100%">
+										<tr class="cabecalho" align="center">
+											<td colspan="2">
+												Compra:
+											</td>
+										</tr>
+										'.$exibirItensVenda.'
+									</table>
+								</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			';
+		}
+		else
+			$conteudo_pagina .= $conteudo_nao_encontrado_full;
+	}
+	else
+		$conteudo_pagina .= '
+			<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="'.$incluir_arquivo.'">
+				<div class="conteudo_box pagina">
+					'.$exibirBuscaNpc.'
+				</div>
+			</div>
+		';
+?>
