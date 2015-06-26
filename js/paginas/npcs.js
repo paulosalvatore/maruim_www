9ea97b824727b16da63268bf635d047f5a28611c
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-25 23:37:33 -0300

    Comentário removido 'npcs.js'

--- js/paginas/npcs.js
+++ js/paginas/npcs.js
@@ -2,7 +2,6 @@ $(function(){
 	$("img.imagemNpc").each(function(){
 		$(this).load(function(){
 			$(this).addClass("largura64");
-			console.log($(this).width());
 			if($(this).width() != 64)
 				$(this).removeClass("largura64");
 		});
