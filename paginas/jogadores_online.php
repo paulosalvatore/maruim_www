eba016cadf328dbd95f0d3e89bdd10678fc93a9c
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-26 23:30:55 -0300

    Página jogadores online criada

---
+++ paginas/jogadores_online.php
@@ -0,0 +1,64 @@
+<?php
+	include("includes/classes/ClassServerStatus.php");
+	$conteudo_pagina .= '
+		<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="'.$pagina.'">
+			<div class="conteudo_box pagina">
+				<div class="box_frame" carregar_box="1">
+					Informações do Servidor
+				</div>
+				<div class="box_frame_conteudo_principal" carregar_box="1">
+					<table width="100%" cellpadding="0" cellspacing="0" class="tabela odd">
+						<tr class="item">
+							<td width="230" class="negrito">
+								Status:
+							</td>
+							<td>
+								'.$ClassServidor->statusServidor().'
+							</td>
+						</tr>
+						'.$ClassServidor->exibirTempoOnline().'
+						<tr class="item">
+							<td class="negrito">
+								Jogadores Online:
+							</td>
+							<td>
+								'.$ClassServidor->pegarNumeroJogadoresOnline().'
+							</td>
+						</tr>
+						<tr class="item">
+							<td class="negrito">
+								Récorde de Jogadores Online:
+							</td>
+							<td>
+								'.$ClassServidor->pegarRecordeOnline().'
+							</td>
+						</tr>
+						<tr class="item">
+							<td class="negrito">
+								Modo de PvP:
+							</td>
+							<td>
+								Aberto
+							</td>
+						</tr>
+						<tr class="item">
+							<td class="negrito">
+								Versão:
+							</td>
+							<td>
+								'.$config["versao"]["exibicao"].'
+							</td>
+						</tr>
+					</table>
+				</div>
+				<br>
+				<div class="box_frame" carregar_box="1">
+					Informações do Servidor
+				</div>
+				<div class="box_frame_conteudo_principal" carregar_box="1">
+					'.$ClassServidor->exibirJogadoresOnline().'
+				</div>
+			</div>
+		</div>
+	';
+?>
