9ebd3c4eec04520952322894fd701fde791986dd
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-26 23:26:42 -0300

    Mmodificação na página da tabela de experiência

--- paginas/tabela_experiencia.php
+++ paginas/tabela_experiencia.php
@@ -10,6 +10,8 @@
 			<div class="conteudo_box pagina padding">
 				Esta é uma lista dos pontos de experiência que são necessários para avançar para os vários níveis. Lembre-se que você também pode verificar '.($id != "profissao" ? "a respectiva barra de habilidade na sua janela de habilidade do cliente" : "a janela de informações da profissão desejada").' para verificar o seu progresso em direção ao próximo nível.<br>
 				<br>
+				Você também pode visualizar a '.($id != "profissao" ? '<a href="?p=tabela_experiencia-profissao">tabela de experiência de profissão</a>' : '<a href="?p=tabela_experiencia">tabela de experiência comum</a>').'.<br>
+				<br>
 				<b>Selecione o nível máximo da tabela:</b><br>
 				<select class="nivelMaximo" style="width: 120px; margin: 3px 0 3px 0;">
 					';
