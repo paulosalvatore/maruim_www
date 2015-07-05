d49d811c7234e5d0430708f81d284290181dc5dc
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-07-05 13:00:12 -0300

    Mudanças de layout na página de chaves de acesso

--- paginas/desenvolvedor_chaves_acesso.php
+++ paginas/desenvolvedor_chaves_acesso.php
@@ -2,11 +2,24 @@
 	$conteudo_pagina .= '
 		<div class="conteudo_pagina" carregar_box="1" carregar_imagem_titulo="">
 			<div class="conteudo_box pagina">
-				<form>
-					<input type="text" id="chave_acesso" onClick="this.select();" />
-					<input type="button" id="gerar_chave_acesso" data-asd="" class="botao" value="Gerar" />
-					<input type="reset" class="botao" value="Limpar" />
-				</form>
+				<table width="100%" cellpadding="0" cellspacing="0" class="tabela">
+					<tr class="cabecalho">
+						<td>
+							Gerar Chave de Acesso
+						</td>
+					</tr>
+					<tr class="item">
+						<td>
+							<textarea id="chave_acesso" onClick="this.select();"></textarea><br>
+							<br>
+							<div align="center">
+								<input type="button" id="gerar_chave_acesso" data-asd="" class="botao" value="Gerar" />
+								<input type="reset" class="botao" value="Limpar" />
+							</div>
+							<br>
+						</td>
+					</tr>
+				</table>
 			</div>
 		</div>
 	';
